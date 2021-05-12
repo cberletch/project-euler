da3c92da0d5dccd65da79f70179d7c2c85d057e4
cberletch <coleberletch@gmail.com>
2021-05-12 12:14:36 -0400

    Update problem-01.py

--- problem-01.py
+++ problem-01.py
@@ -1,9 +1,7 @@
 def multiples35(num):
     sum = 0
     for i in range(num):
-        if (i % 3 == 0):
-            sum += i
-        elif (i % 5 == 0):
+        if (i % 3 == 0) or (i % 5 == 0):
             sum += i
     return sum
 
